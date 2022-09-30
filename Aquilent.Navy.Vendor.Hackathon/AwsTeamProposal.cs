814f061a2b2b56f45e4febb9d71da9126f085a56
Tundey <9803394+tundey@users.noreply.github.com>
2022-09-29 23:05:18 -0400

    Add files via upload
    Initial commit

---
+++ Aquilent.Navy.Vendor.Hackathon/AwsTeamProposal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquilent.Navy.Vendor.Hackathon
+{
+    public partial class AwsTeamProposal
+    {
+        public int OpportunityId { get; set; }
+        public string TeamProposalId { get; set; }
+        public int ProposalId { get; set; }
+        public string PrimeCompany { get; set; }
+        public string ContractNumber { get; set; }
+        public string Company { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string BidderName { get; set; }
+        public bool IsPrime { get; set; }
+        public int PrimeCompanyId { get; set; }
+        public int ContractId { get; set; }
+        public int CompanyId { get; set; }
+        public int BidderId { get; set; }
+        public bool IsSuperseded { get; set; }
+    }
+}
